cdd44f99b567b76339c217497c3f4f924b86187d
Shrdz <shraddhayadav146@gmail.com>
2020-08-19 21:50:21 +0530

    Updates

---
+++ precache-manifest.e8df44fb6c359800db37045e4f244e36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34dbde49e9af6f074922b6969d455dea",
+    "url": "/Currency-Concertor/index.html"
+  },
+  {
+    "revision": "21ae8519c3daf9e631b3",
+    "url": "/Currency-Concertor/static/css/main.9041370b.chunk.css"
+  },
+  {
+    "revision": "667a134865e61eac26d7",
+    "url": "/Currency-Concertor/static/js/2.ae8eebfc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Currency-Concertor/static/js/2.ae8eebfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21ae8519c3daf9e631b3",
+    "url": "/Currency-Concertor/static/js/main.6c459261.chunk.js"
+  },
+  {
+    "revision": "0835f7222e6a2c2f49f4",
+    "url": "/Currency-Concertor/static/js/runtime-main.9d48e197.js"
+  }
+]);
